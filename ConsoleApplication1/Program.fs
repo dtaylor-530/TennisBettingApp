9217336ec18cee5f4151797f7d14bd4fafb338d8
dtaylor-530 <d.taylor530@gmail.com>
2019-10-09 16:18:19 +0100

    fixed display of tournaments in legend

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -11,23 +11,23 @@ let getModifiedPoints (points:seq<datapoints.DataPoint>)=
                               let w = point.weight
                               yield (m*w*100L/v, point.ratio)} |> Seq.toArray
 
-let getChart y (points:seq<datapoints.DataPoint>) i=  let y2 = seq{ for point in points do 
+let getLine y (points:seq<datapoints.DataPoint>) i = let y2 = seq{ for point in points do 
                                                                        yield (seq{for (p,m) in y do
                                                                                     let diff =1.0 /(Math.Pow((Math.Abs(point.ratio-m) + i |> float),3.0))
                                                                                     let xx = diff * (p |> float)
                                                                                     yield xx}
                                                         |> Seq.average,point.ratio)
                                                                          }
-                                                      let chart = Chart.Line [for (i2,i3) in y2 -> (i3, i2)] 
-                                                      chart
+                                                     [for (i2,i3) in y2 -> (i3, i2)]  
+                                                 
 
 [<EntryPoint>]
 let main argv = 
     let arr = [|for atp in 5L..15L do 
-                 let points = TennisBetting.datapoints.getAllPoints |> Seq.filter (fun x -> x.atp = atp) 
-                 let chart= getChart (getModifiedPoints points) points atp
-                 yield chart.WithLegend(true, tournaments.getTournamentName(atp))|]
-    let combine = Chart.Combine(arr)
+                    let points = TennisBetting.datapoints.getAllPoints |> Seq.filter (fun x -> x.atp = atp) 
+                    let chart= Chart.Line(getLine (getModifiedPoints points) points atp, tournaments.getTournamentName(atp))
+                    yield chart|]
+    let combine = Chart.Combine(arr).WithLegend(true)
     Chart.Show combine 
     0
 
